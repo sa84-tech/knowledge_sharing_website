bbab69de4ffa654e7d3cb4e22a4d77763fe09cfd
Андрей Громов <sa84@list.ru>
2022-04-25 10:55:38 +0300

    custom templator tag for avatar added

--- ksw/mainapp/templatetags/main_extras.py
+++ ksw/mainapp/templatetags/main_extras.py
@@ -5,7 +5,11 @@ register = template.Library()
 
 @register.simple_tag
 def check_like(post, user):
-    print('###### post', post)
     if post.is_liked_by(user):
         return 'fa-solid'
     return 'fa-regular'
+
+
+@register.simple_tag
+def get_avatar(user):
+    return user.avatar if user.avatar else '/media/seeder/users/no_avatar.jpg'
