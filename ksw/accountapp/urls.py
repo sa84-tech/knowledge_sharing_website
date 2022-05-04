2b7d0ecf056c97c1f38a574cae5477bb68c82caa
sa84 <sa84@list.ru>
2022-05-05 01:11:11 +0300

    settings page added

--- ksw/accountapp/urls.py
+++ ksw/accountapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import account, post_create, post_read, post_update, post_delete
+from .views import account, settings, post_create, post_read, post_update, post_delete
 
 app_name = 'accountapp'
 
 urlpatterns = [
     path('lk/', account, name='lk'),
+    path('settings/', settings, name='settings'),
     path('post_create/', post_create, name='post_create'),
     path('post_read/', post_read, name='post_read'),
     path('post_update/', post_update, name='post_update'),
--- ksw/ksw/static/js/tabs.js
+++ ksw/ksw/static/js/tabs.js
@@ -1,39 +1,46 @@
 'use strict'
 
-const triggerTabList = [...document.querySelectorAll('#pills-tab button')];
-const triggeredTabPanes = [...document.querySelectorAll('.switchable-tab')];
-const filterBtnGroup = document.querySelector('#filter-btn-group')
+document.addEventListener('DOMContentLoaded', function (event) {
+    const triggerTabList = [...document.querySelectorAll('#pills-tab button')];
+    const triggeredTabPanes = [...document.querySelectorAll('.switchable-tab')];
+    const filterBtnGroup = document.querySelector('#filter-btn-group')
 
-console.log(filterBtnGroup)
+    console.log(filterBtnGroup)
 
-triggerTabList.forEach(function (triggerEl) {
-    const tabTrigger = new bootstrap.Tab(triggerEl);
-    triggerEl.addEventListener('click', function (event) {
-        event.preventDefault();
-        tabTrigger.show();
+    triggerTabList.forEach(function (triggerEl) {
+        const tabTrigger = new bootstrap.Tab(triggerEl);
+        triggerEl.addEventListener('click', function (event) {
+            event.preventDefault();
+            tabTrigger.show();
 
-        triggeredTabPanes.forEach(item => {
-            item.classList.remove('show');
-            item.classList.remove('active');
-            if (item.id.includes(event.target.dataset.bsTarget.slice(1))) {
-                item.classList.add('show');
-                item.classList.add('active');
-            }
+            triggeredTabPanes.forEach(item => {
+                item.classList.remove('show');
+                item.classList.remove('active');
+                if (item.id.includes(event.target.dataset.bsTarget.slice(1))) {
+                    item.classList.add('show');
+                    item.classList.add('active');
+                }
+            });
         });
     });
+
+
+
+//    filterBtnGroup.addEventListener('click', function (event) {
+//        if (event.target.id.includes('filter-posts')) {
+//            console.log(event.target.dataset.filter)
+//        }
+//        else if (event.target.id.includes('filter-comments')) {
+//            console.log(event.target.dataset.filter)
+//        }
+//        else if (event.target.id.includes('filter-bookmarks')) {
+//            console.log(event.target.dataset.filter)
+//        }
+//        else if (event.target.id.includes('filter-subscriptions')) {
+//            console.log(event.target.dataset.filter)
+//        }
+//    });
+
 });
 
-filterBtnGroup.addEventListener('click', function (event) {
-    if (event.target.id.includes('filter-posts')) {
-        console.log(event.target.dataset.filter)
-    }
-    else if (event.target.id.includes('filter-comments')) {
-        console.log(event.target.dataset.filter)
-    }
-    else if (event.target.id.includes('filter-bookmarks')) {
-        console.log(event.target.dataset.filter)
-    }
-    else if (event.target.id.includes('filter-subscriptions')) {
-        console.log(event.target.dataset.filter)
-    }
-});
+
