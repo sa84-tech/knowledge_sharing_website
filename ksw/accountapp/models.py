1fce05c75ee3d61496f840d5af721ab00213f31a
sa84 <sa84@list.ru>
2022-05-14 02:27:17 +0300

    bookmark model created, import script updated

--- ksw/accountapp/models.py
+++ ksw/accountapp/models.py
@@ -1,3 +1,24 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Bookmark(models.Model):
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+    )
+
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Закладка'
+        verbose_name_plural = 'Закладки'
