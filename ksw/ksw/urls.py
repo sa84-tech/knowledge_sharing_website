172e3778b4a52c26db76547d7d7facb2bc8d169e
dpaklin <t5tchq1G1DAigtuYuvyn>
2022-05-23 15:51:29 +0300

    simple search

--- ksw/ksw/urls.py
+++ ksw/ksw/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
-from mainapp.views import index_page, post_page, add_comment, add_like, add_bookmark
+from mainapp.views import index_page, post_page, add_comment, add_like, add_bookmark, search
 
 
 urlpatterns = [
@@ -33,4 +33,5 @@ urlpatterns = [
     path('auth/', include('authapp.urls', namespace='auth'), name='auth'),
     path('account/', include('accountapp.urls', namespace='account'), name='account'),
     path('', include('social_django.urls', namespace='social')),
+    path('search/', search, name='search'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
