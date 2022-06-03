f6a3bfa396183b0602067b6b1f49bc03a6a0504d
Андрей Громов <sa84@list.ru>
2022-06-03 18:32:15 +0300

    issue with running tests fixed

--- ksw/authapp/tests.py
+++ ksw/authapp/tests.py
@@ -4,20 +4,34 @@ from django.core.management import call_command
 from authapp.models import WriterUser
 
 
-
 class TestAuthappSmoke(TestCase):  # в .env DEBUG=False
     def setUp(self):
         call_command('flush', '--noinput')
-        call_command('loaddata', 'init_data.json')
+        call_command('loaddata', 'test_db.json')
         self.client = Client()
 
         self.superuser = WriterUser.objects.create_superuser('super_test', 'super_django@test.local', '123test')
-        self.superuser = WriterUser.objects.create_user('user1_test', 'user1_django@test.local', '123test')
-        self.superuser = WriterUser.objects.create_user('user2_test', 'user2_django@test.local', '123test',
-                                                        first_name='user2')
+        self.user = WriterUser.objects.create_user('user1_test', 'user1_django@test.local', '123test')
+        self.user_with__name = WriterUser.objects.create_user('user2_test', 'user2_django@test.local', '123test',
+                                                              first_name='John', last_name='Dow')
 
     def test_user_login(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.context['user'].is_anonymous)
-        self.assertEqual(response.context['title'], 'Главная')
+        self.assertEqual(response.context['title'], 'Главная страница')
+        self.assertNotContains(response, f'@{self.user.username}', status_code=200)
+
+        self.client.login(username='user1_test', password='123test')
+
+        response = self.client.get('/auth/login/')
+        self.assertFalse(response.context['user'].is_anonymous)
+        self.assertEqual(response.context['user'], self.user)
+
+        response = self.client.get('/')
+        self.assertContains(response, f'@{self.user.username}', status_code=200)
+        self.assertEqual(response.context['user'], self.user)
+
+
+    def tearDown(self):
+        call_command('sqlsequencereset', 'mainapp', 'authapp')
--- ksw/mainapp/tests.py
+++ ksw/mainapp/tests.py
@@ -1,38 +1,21 @@
 from django.test import TestCase
 from django.test.client import Client
 from django.core.management import call_command
-from ksw.mainapp.models import Category
-from django.test import SimpleTestCase
-from django.urls import reverse, resolve
-from django.urls import path, include, reverse_lazy
-from django.contrib.auth import views as auth_views
-from ksw.authapp.views import login, logout, register, edit, verify, PasswordChangeView, password_success, \
-    email_change, email_success, PassResetView
-from mainapp.views import index_page
-
-
-class TestMainUrls(SimpleTestCase):
-
-    def test_home_url_response(self):
-        response = self.client.get('/')
-        print('answer<<<<<<<<')
-        self.assertEqual(response.status_code, 200)
+from mainapp.models import Category
 
 
 class TestMainappSmoke(TestCase):  # в .env DEBUG=False
     def setUp(self):
         call_command('flush', '--noinput')
-        call_command('loaddata', 'init_data.json')
+        call_command('loaddata', 'test_db.json')
         self.client = Client()
 
     def test_mainapp_urls(self):
-
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
-        for category in Category.objects.all():
-            response = self.client.get(f'/{category.slug}')
-            self.assertEqual(response.status_code, 200)
+        response = self.client.get('/category/design')
+        self.assertEqual(response.status_code, 200)
 
-
-
+    def tearDown(self):
+        call_command('sqlsequencereset', 'mainapp', 'authapp')
