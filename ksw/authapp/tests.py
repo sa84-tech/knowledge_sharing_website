1374f10bbe6d676af4e9c8b7ea68ff8d8a596da9
69SomeoneElse69 <fatal.critical.error@gmail.com>
2022-06-05 14:30:57 +0300

    Tests v1

--- ksw/authapp/tests.py
+++ ksw/authapp/tests.py
@@ -3,6 +3,8 @@ from django.test.client import Client
 from django.core.management import call_command
 from authapp.models import WriterUser
 
+from mainapp.models import Post
+
 
 class TestAuthappSmoke(TestCase):  # в .env DEBUG=False
     def setUp(self):
@@ -16,22 +18,68 @@ class TestAuthappSmoke(TestCase):  # в .env DEBUG=False
                                                               first_name='John', last_name='Dow')
 
     def test_user_login(self):
+        # доступность главной страницы для не авторизированного пользователя
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.context['user'].is_anonymous)
         self.assertEqual(response.context['title'], 'Главная страница')
         self.assertNotContains(response, f'@{self.user.username}', status_code=200)
 
+        # данные пользователя
         self.client.login(username='user1_test', password='123test')
 
+        # авторизируемся на сайте
+        self.assertFalse(response.context['user'].is_active)
         response = self.client.get('/auth/login/')
         self.assertFalse(response.context['user'].is_anonymous)
         self.assertEqual(response.context['user'], self.user)
+        self.assertTrue(response.context['user'].is_active)
 
+        # доступность главной страницы для авторизированного пользователя
         response = self.client.get('/')
         self.assertContains(response, f'@{self.user.username}', status_code=200)
         self.assertEqual(response.context['user'], self.user)
 
+        # доступность страницы помощи для авторизированного пользователя
+        response = self.client.get('/help/')
+        self.assertContains(response, f'@{self.user.username}', status_code=200)
+        self.assertEqual(response.context['user'], self.user)
+
+        # доступность админки для авторизированного пользователя
+        response = self.client.get('/admin/')
+        self.assertNotEqual(response.status_code, 200)
+
+        # доступность аккаунта для зарегистрированного пользователя
+        response = self.client.get(f'/account/users/{self.user.username}/')
+        self.assertEqual(response.status_code, 200)
+
+        for post in Post.objects.all():
+            if post.status != "draft":
+                # доступность чужого "черновика" для зарегистрированного пользователя
+                response = self.client.get(f'/post/{post.pk}')
+                self.assertEqual(response.status_code, 200)  # ПОМЕНЯТЬ! на assertNotEqual после слива веток
+            elif post.status != 'under_review':
+                # доступность чужой "статьи на модерации" для зарегистрированного пользователя
+                response = self.client.get(f'/post/{post.pk}')
+                self.assertEqual(response.status_code, 200)  # ПОМЕНЯТЬ! на assertNotEqual после слива веток
+            elif post.status != 'deleted':
+                # доступность чужой "удаленной статьи" для зарегистрированного пользователя
+                response = self.client.get(f'/post/{post.pk}')
+                self.assertEqual(response.status_code, 200)  # ПОМЕНЯТЬ! на assertNotEqual после слива веток
+
+            # доступность чужой опубликованной статьи для зарегистрированного пользователя
+            response = self.client.get(f'/post/{post.pk}')
+            self.assertEqual(response.status_code, 200)
+
+        # выход из учетной записи
+        self.client.logout()
+
+        # доступность админки для не зарегистрированного пользователя
+        response = self.client.get('/admin/')
+        self.assertNotEqual(response.status_code, 200)
+        # доступность аккаунта для не зарегистрированного пользователя
+        response = self.client.get(f'/account/users/{self.user.username}/')
+        self.assertNotEqual(response.status_code, 200)
 
     def tearDown(self):
         call_command('sqlsequencereset', 'mainapp', 'authapp')
--- ksw/mainapp/tests.py
+++ ksw/mainapp/tests.py
@@ -1,20 +1,36 @@
 from django.test import TestCase
 from django.test.client import Client
 from django.core.management import call_command
-from mainapp.models import Category
+
+from mainapp.models import Category, Post
 
 
-class TestMainappSmoke(TestCase):  # в .env DEBUG=False
+class TestMainappSmoke(TestCase):
     def setUp(self):
         call_command('flush', '--noinput')
         call_command('loaddata', 'test_db.json')
         self.client = Client()
 
     def test_mainapp_urls(self):
+        # проверка стнаницы помощи
+        response = self.client.get('/help/')
+        self.assertEqual(response.status_code, 200)
+
+        # проверка главной страницы
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
-        response = self.client.get('/category/design')
+        # проверка всех категорий
+        for category in Category.objects.all():
+            response = self.client.get(f'/category/{category.slug}')
+            self.assertEqual(response.status_code, 200)
+
+        # проверка всех статей
+        for post in Post.objects.all():
+            response = self.client.get(f'/post/{post.pk}')
+
+        # проверка станицы логирования
+        response = self.client.get('/auth/')
         self.assertEqual(response.status_code, 200)
 
     def tearDown(self):
